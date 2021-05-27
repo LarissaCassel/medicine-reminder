595c185be63838013ccc213dcea010aa727270d6
Larissa Cassel <lari.cassel@gmail.com>
2021-05-27 17:39:35 -0300

    Lottie Animation

---
+++ src/components/Load.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import LottieView from 'lottie-react-native';
+
+export function Load(){
+    return(
+      <View style = {styles.container} >
+          <LottieView
+              source={require('../assets/medicinepills.json')}
+              autoPlay
+              style = {styles.animation}
+          />
+      </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+container:{
+flex:1,
+justifyContent:"center",
+alignItems:'center',
+},
+animation:{
+width:200,
+height:200,
+}
+
+});
