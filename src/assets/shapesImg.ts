1c63388a85aa5da649174155cf3e8e3223e8bea8
Larissa Cassel <lari.cassel@gmail.com>
2021-05-28 02:10:21 -0300

    Pill Aparence List

---
+++ src/assets/shapesImg.ts
@@ -0,0 +1,23 @@
+const shapes = [
+    {name: 'pill', 
+     img:'https://images.gamebanana.com/img/ico/sprays/50f916fcc9c5b.png', 
+     id: 0,
+     key: 'djdjisiso'
+     },
+    {name: 'Pill', 
+     img: 'https://images.gamebanana.com/img/ico/sprays/50f916fcc9c5b.png', 
+     id: 1,
+     key: 'dhhsksho'
+     },
+    {name: 'Pill', 
+     img: 'https://images.gamebanana.com/img/ico/sprays/50f916fcc9c5b.png', 
+     id: 2,
+     key: 'sipfioiso'
+      },
+      {name: 'Pill', 
+     img: 'https://images.gamebanana.com/img/ico/sprays/50f916fcc9c5b.png', 
+     id: 3,
+     key: 'skskowwoid'
+      }
+  ];
+  export default shapes;
